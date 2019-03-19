4bf07749aae089ca5d63356b46d9649df504cf48
marcella hadderingh <marcellavanduyn@gmail.com>
2019-03-19 11:56:01 +0100

    template + style Todolist.vue
    first start

---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs
+    to be done" v-model="newTodo">
+    Todo list goes here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .todo-input{
+    widows:100% ;
+    padding: 10px 10px;
+    font-size: 18px;
+    margin-bottom:16px;
+
+    &:focus{
+      outline:0;
+    }
+  }
+</style>
