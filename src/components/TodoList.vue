b5f0924299e34e66fc77ef6fb28aca1a5c1792bd
marcella hadderingh <marcellavanduyn@gmail.com>
2019-03-19 12:27:46 +0100

    Methods
    adding input to list. (NO DB)

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <input type="text" class="todo-input" placeholder="What needs
-    to be done" v-model="newTodo">
+    to be done" v-model="newTodo" @keyup.enter="addTodo">
    <div v-for="todo in todos" :key="todo.id" class="todo-item">
      {{todo.title }}
    </div>
@@ -14,18 +14,31 @@ export default {
   data () {
     return {
      newTodo: '',
+     idForTodo: 3,
      todos: [
        {
-         'id':1,
+         'id': 1,
          'title': 'Finish Vue Screencast',
          'completed': false,
        },
         {
-         'id':2,
+         'id': 2,
          'title': 'Take over world',
          'completed': false,
        },
      ]
+    }
+  },
+  methods: {
+    addTodo (){
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      })
+
+      this.newTodo = ''
+      this.idForTodo++
     }
   }
 }
