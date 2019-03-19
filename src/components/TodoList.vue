62763051a793147d5931a1d75397e9a63b21dec9
marcella hadderingh <marcellavanduyn@gmail.com>
2019-03-19 12:01:27 +0100

    script and data

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,9 @@
   <div>
     <input type="text" class="todo-input" placeholder="What needs
     to be done" v-model="newTodo">
-    Todo list goes here
+   <div v-for="todo in todos" :key="todo.id" class="todo-item">
+     {{todo.title }}
+   </div>
   </div>
 </template>
 
@@ -11,7 +13,19 @@ export default {
   name: 'todo-list',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+     newTodo: '',
+     todos: [
+       {
+         'id':1,
+         'title': 'Finish Vue Screencast',
+         'completed': false,
+       },
+        {
+         'id':2,
+         'title': 'Take over world',
+         'completed': false,
+       },
+     ]
     }
   }
 }
